af4d14efaa737349565b9bd376b973a64b0337f1
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-04-12 10:36:26 -0300

    chore Basic setup

---
+++ src/styles/theme.ts
@@ -0,0 +1,29 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      '50': '#F5F8FA',
+      '200': '#DADADA',
+      '500': '#47585B',
+      '700': '#999999',
+      '800': '#47585B',
+    },
+    yellow: {
+      '500': '#FFBA08',
+    },
+  },
+
+  fonts: {
+    body: 'Poppins',
+  },
+
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.50',
+        color: 'gray.500',
+      },
+    },
+  },
+});
